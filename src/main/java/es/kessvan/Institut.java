0759db282342527e0ed6934baa6b07e92e27798e
kessy7u7 <kessvan.one@gmail.com>
2022-03-20 15:46:15 +0100

    Activitat 3 - Extraer constants

--- src/main/java/es/kessvan/Institut.java
+++ src/main/java/es/kessvan/Institut.java
@@ -2,14 +2,19 @@ package es.kessvan;
 
 public class Institut {
 
+	private static final int ANY = 2021;
+	private static final String CICLE_FORMATIU = "1º DAW";
+	private static final String COGNOMS_ALUMNE = "Cedeño";
+	private static final String NOM_ALUMNE = "Kessvan";
+
 	public static void main(String[] args) {
 		
 		Alumne alumne = new Alumne();
 		
-		alumne.nom = "Kessvan";
-		alumne.cognoms = "Cedeño";
-		alumne.cicleFormatiu = "1º DAW";
-		alumne.any = 2021;
+		alumne.nom = NOM_ALUMNE;
+		alumne.cognoms = COGNOMS_ALUMNE;
+		alumne.cicleFormatiu = CICLE_FORMATIU;
+		alumne.any = ANY;
 		
 		System.out.println(alumne.toString());
 
